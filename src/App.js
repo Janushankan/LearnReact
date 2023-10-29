1bb0f00fefb8892964f75ed2214eeeb1d00b537b
V. Janushankan <vj.janushankan1006@gmail.com>
2023-10-29 20:23:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  function handleNameChange() {
+    const names = ["Earn", "Grow", "Give"];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Subscribe to Janu
+      <p>Let's {handleNameChange()} Money</p>
     </div>
   );
 }
