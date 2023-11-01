f4dee7c2c5980094217a18a84212220159f4a0fc
V. Janushankan <vj.janushankan1006@gmail.com>
2023-11-01 18:14:09 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,31 +3,25 @@ import Header from "./Header";
 import Content2 from "./Content2";
 import Footer from "./Footer";
 import { useState } from 'react';
-
+import AddItem from "./AddItem";
+import SearchItem from "./SearchItem";
 
 
 
 function App() {
 
-  const [items, setItems] = useState(
-    [
-      {
-        id: 1,
-        checked: true,
-        item: "Practice Coding"
-      },
-      {
-        id: 2,
-        checked: false,
-        item: "Play Cricket"
-      },
-      {
-        id: 3,
-        checked: false,
-        item: "Read about AI"
-      }
-    ]
-  );
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('todo_list')));
+
+  const [newItem, setNewItem] = useState('')
+  const [search, setSearch] = useState('')
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1
+    const addNewItem = { id, checked: false, item }
+    const listItems = [...items, addNewItem]
+    setItems(listItems)
+    localStorage.setItem("todo_list", JSON.stringify(listItems))
+  }
 
   const handleCheck = (id) => {
     // console.log(`id: ${id}`)
@@ -40,15 +34,31 @@ function App() {
     const listItems = items.filter((item) => item.id !== id)
     setItems(listItems)
     localStorage.setItem("todo_list", JSON.stringify(listItems))
+  }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!newItem) return;
+    console.log(newItem)
+    addItem(newItem)
+    setNewItem('')
   }
 
   return (
     <div className="App">
       <Header title="Janushankan" />
       {/* <Content1 /> */}
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
+      <SearchItem 
+        search = {search}
+        setSearch = {setSearch}
+      />
       <Content2
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
